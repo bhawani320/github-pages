6623c402576016501b77be78ccc8e3ffb1d14183
bhawani320 <edugovtjob@gmail.com>
2024-12-26 13:24:58 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,220 @@
+<html lang="en-IN" itemscope="" itemtype="http://schema.org/WebPage"><head>
+<meta charset="UTF-8">
+<title itemprop="name">Wow Speed Post Tracking India - postal parcel track </title>
+<style>
+img.play{padding: 0;margin: 0;border: none;width: 10px;}
+.amaz{padding: 0;margin: 0;width: 50px; display: inline-block;border: none;}
+.ifr{background: url(/Content/preload.gif) center center no-repeat;}
+</style>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<link href="https://parceltrack.in/images/logo3.png" rel="shortcut icon" type="image/png">
+<style>
+.alink {
+font-size: 14px;
+color: blue !important;
+text-decoration: underline;
+}
+iframe {
+width: 100% !important;
+-webkit-transform: initial !important;
+}
+.form-wrap-left {
+text-align: center;
+margin: 10px auto;
+padding: 10px 0;
+}
+.form-wrap-left input {
+width: 270px;
+}
+.form-wrap-right {
+}
+</style>
+<style>
+a.featuredpic img{margin-top:4px;padding:initial;border:none}body,html{margin:0;padding:0}aside ul,fieldset ol,footer ul,ul#menu li{list-style:none}#body,.clear,.message-error,.message-info,footer{clear:both}html{background-color:#b52a25}body{background-color:#fff;border-top:solid 2px #000;color:#b52a25;font-family:Tahoma,Verdana,Segoe,sans-serif}header{background-color:#b52a25;color:#fff}.tweets{display:block;margin:10px 0}.tweet{display:block;padding:10px}.tweet .left{width:10%;float:left}.tweet .right{width:89%;float:left}.tweet .right img{display:none}.tweet img{max-width:300px}.tweets .alt{background:#fff}span.pretty-date{font-size:.6em}.btn{display:inline-block;margin-bottom:0;font-weight:400;text-align:center;vertical-align:middle;-ms-touch-action:manipulation;touch-action:manipulation;cursor:pointer;background-image:none;border:1px solid transparent;white-space:nowrap;padding:6px;font-size:13px;line-height:1.42857143;border-radius:4px;-webkit-user-select:none;-moz-user-select:none;-ms-user-select:none;user-select:none}.btn-primary{color:#fff;background-color:#000000;border-color:#204d74;font-size:18px;margin:0 0 5px;padding:5px}.btn-twitter{background-color:#00acee!important}.btn-facebook{background-color:#3b5998!important}.btn-googleplus{background-color:#e93f2e!important}.btn-socal{font-size:small;color:#fff!important;margin:5px 0;transition:background-color .25s ease;border-radius:0;padding:8px 20px!important}a,a:active,a:hover,a:link,a:visited{color:#60f}.ads{margin:10px 0}.ads h6{font-weight:200;padding:2px 0;margin:0}#frame{width:100%;height:500px;border:1px solid #000}.form-head{padding:0 0 0 6px;margin:0 0 5px}a{outline:0;padding-left:3px;padding-right:3px;text-decoration:underline}a:hover{background-color:#0f0e0e}footer,header,hgroup,nav,section{display:block}img{margin:10px 0;border:1px solid silver;padding:5px}mark{background-color:#a6dbed;padding-left:5px;padding-right:5px}.float-left{float:left}.float-right{float:right}.clear-fix:after{content:".";clear:both;display:block;height:0;visibility:hidden}h1,h2,h3,h4,h5,h6{color:#000;margin:0;padding:0}h1{font-size:2em}h2{font-size:22px;color:#000000}h3{font-size:1.2em}h4{font-size:1.1em}h5,h6{font-size:1em}h5 a:active,h5 a:link,h5 a:visited{padding:0;text-decoration:none}.content-wrapper{margin:0 auto;max-width:950px}.content-wrapper h1{text-align:center;font-size:25px;color:#000000;text-transform:uppercase;margin-top:15px}#body{background-color:#fff;padding-bottom:35px}.main-content{padding-left:10px}header .content-wrapper{padding:5px 0}footer{background-color:#b52a25;font-size:.8em;padding:10px 0}footer ul{display:inline-flex;padding:0}hgroup.title h1,hgroup.title h2,ul#menu li{display:inline}footer ul li{margin-right:10px}em{background-color:#b52a25}.site-title{font-weight:bolder;font-size:2em;margin:0}.site-title img{height:40px;margin:0;border:none;padding:0}.site-title a,.site-title a:active,.site-title a:hover{background:0 0;color:#fff;outline:0;text-decoration:none}ul#menu li a,ul#menu li a:hover{color:#fff}ul#menu{font-size:1em;margin:0 0 5px;padding:0;text-align:right}ul#menu li{padding-left:10px}ul#menu li a{background:0 0;text-decoration:underline!important}.featured{background-color:#ececec}.featured .content-wrapper{background-color:#dde8f9;color:#02233c;padding:15px 0 0}.featured hgroup.title h1,.featured hgroup.title h2{color:#fff}.featured p{font-size:1.1em}.contact h3,input,label,textarea{font-size:1.2em}hgroup.title{margin-bottom:10px}hgroup.title h2{font-weight:400;margin-left:3px}section.feature{width:300px;float:left;padding:10px}ol.round{list-style-type:none;padding-left:0}ol.round li{margin:25px 0;padding-left:45px}aside ul,fieldset,fieldset ol{padding:0}.contact iframe,.contact p{margin:5px 0 0 10px}.label{font-weight:700}.contact iframe{border:1px solid #fff}form{display:inline-block}fieldset{border:none;margin:0}fieldset legend{display:none}fieldset ol li{padding-bottom:5px}label{display:block;font-weight:600}label.checkbox{display:inline}.field-validation-valid,.validation-summary-valid{display:none}input,textarea{margin:5px;padding:5px}textarea{font-family:inherit;width:500px}input:focus,textarea:focus{border:1px solid #7ac0da}input[type=radio]{background:0 0;border:inherit;width:auto}button,input[type=button],input[type=submit]{background-color:#d3dce0;border:1px solid #787878;cursor:pointer;font-size:1.2em;font-weight:600;padding:7px;margin-right:8px;width:auto}.field-validation-error,.validation-summary-errors{font-weight:700;color:#e80c4d}td button,td input[type=button],td input[type=submit]{font-size:1em;padding:4px;margin-right:4px}.message-error,.message-success{margin:20px 0 10px;font-weight:700}.message-info{border:1px solid;padding:10px 20px}.message-error{color:#e80c4d;font-size:1.1em}.message-success{color:#7ac0da;font-size:1.3em}.error{color:#e80c4d}input.input-validation-error{border:1px solid #e80c4d}input[type=checkbox].input-validation-error,table,td,th{border:0}.validation-summary-errors{font-size:1.1em}table{border-collapse:collapse;border-spacing:0;margin-top:.75em}th{font-size:1.2em;text-align:left;padding-left:0}th a{display:block;position:relative}th a:active,th a:hover,th a:link,th a:visited{color:#fff;font-weight:600;text-decoration:none;padding:0}th a:hover{color:#000}th.asc a,th.desc a{margin-right:.75em}th.asc a:after,th.desc a:after{display:block;position:absolute;right:0;top:0;font-size:.75em;content:'�'}tr.pager td{padding:0 .25em 0 0}table,td,th{border:1px solid silver!important;padding:5px!important}.left-side{width:65%;float:left}.right-side{width:35%;float:left}@media only screen and (max-width:850px){image{width:100%}.form-wrap-left,.form-wrap-right,.left-side,.right-side{width:100%;float:none}article,aside,footer .float-left,footer .float-right,header .float-left,header .float-right{float:none}footer,header .site-title,ul#menu{text-align:center}#frame{width:475px;height:500px;border:1px solid #000;-ms-zoom:.75;-moz-transform:scale(.75);-moz-transform-origin:0 0;-o-transform:scale(.75);-o-transform-origin:0 0;-webkit-transform:scale(.75);-webkit-transform-origin:0 0}img{width:98%;padding:0;margin:10px 0;object-fit:scale-down}.tweet .left img{width:19px!important;border:1px solid silver;padding:2px}ul#menu,ul#menu li{margin:0;padding:0}nav{margin-bottom:5px}.featured .main-content,.main-content{background-position:10px 0}.content-wrapper{padding-right:10px;padding-left:10px}.featured .content-wrapper{padding:10px}article,aside{width:100%}ol.round{list-style-type:none;padding-left:0}ol.round li{padding-left:10px;margin:25px 0}ol.round li.eight,ol.round li.five,ol.round li.four,ol.round li.nine,ol.round li.one,ol.round li.seven,ol.round li.six,ol.round li.three,ol.round li.two,ol.round li.zero{background:0 0}section.feature{float:none;padding:10px;width:auto}section.feature img{color:#999;content:attr(alt);font-size:1.5em;font-weight:600}form{width:100%;display:block;padding:0}input{width:62%}footer{height:auto;padding:10px 0}footer p{margin:0}a.featuredpic{float:left;display:inline-block}}
+</style>
+</head>
+<body>
+<header>
+<nav class="content-wrapper">
+<div class="float-left">
+<p class="site-title">
+<a href="/" title="speed post">
+<img alt="postal parcel track" src="https://parceltrack.in/images/logow3.png">
+</a>
+</p>
+</div>
+<div class="clear"></div>
+</nav>
+</header>
+<main id="body">
+<div class="content-wrapper main-content clear-fix">
+<section class="row">
+<h1>postal parcel track</h1>
+<p>
+There is a worldwide courier company in the country, and parcels from all over the world come and go everyday, in such a situation, the problem of tracking the parcel also arises, in such a situation, you can use this website to do the parcel. Is, which tracks the parcel of every courier company and gives it to you.
+</p>
+<div>
+</div>
+</section>
+</div>
+<section class="content-wrapper">
+<div class="featured">
+<div class="form-wrap-left">
+<form action="http://localhost/trial/api.php" method="POST">
+<input type="text" name="tracking" required="" placeholder="enter tracking no." autocomplete="off" value=""><input type="submit" name="submit" style="background-color: #ff0000;
+border: 1px solid #ffffff;" class="btn btn-primary " value="✔ Track &nbsp;"/>
+</form>
+</div>
+</div>
+</section>
+<?php
+error_reporting(0);
+if($_POST['submit']==true){
+$curl = curl_init();
+curl_setopt_array($curl, array(
+CURLOPT_URL => 'https://api.dhboss.com/apicall/consignment_tracker/',
+CURLOPT_RETURNTRANSFER => true,
+CURLOPT_ENCODING => '',
+CURLOPT_MAXREDIRS => 10,
+CURLOPT_TIMEOUT => 0,
+CURLOPT_FOLLOWLOCATION => true,
+CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+CURLOPT_CUSTOMREQUEST => 'POST',
+CURLOPT_POSTFIELDS => 'track_number='.$_POST['tracking'].'&apikeyfill=apikey',
+));
+$response = curl_exec($curl);
+curl_close($curl);
+$parceljson = json_decode($response,true);
+echo '<script>alert("'.$parceljson['message'].'");</script>';
+//echo '<pre>';
+//print_r($parceljson);
+//echo '</pre>';
+?>
+<section class="section section-sm section-half-bg novi-bg novi-bg-img section-transparent" style="background-position: center center; background-repeat: no-repeat; background-size: cover; box-sizing: border-box; padding: 0px 0px 85px; position: relative;">
+<div class="container" style="box-sizing: border-box; margin-left: auto; margin-right: auto; max-width: 720px; padding-left: 15px; padding-right: 15px; width: 100%;">
+<div class="card-group-custom card-group-custom-with-shadow" aria-multiselectable="false" id="accordion1" role="tablist" style="box-shadow: rgba(26, 28, 36, 0.1) 4px 20px 14px; box-sizing: border-box; margin-bottom: 0px;">
+<article class="card card-custom card-classic">
+<div class="card-header" id="Indiapost" role="tab" style="background-attachment: initial; background-clip: initial; background-image: initial; background-origin: initial; background-position: initial; background-repeat: initial; background-size: initial; border-bottom: 0px; border-radius: 0px; box-sizing: border-box; margin-bottom: 0px; padding: 0px;">
+<div class="card-title" style="box-sizing: border-box; margin-bottom: 0px;">
+<a aria-controls="accordion1-card-body-kgiydcoi" aria-expanded="true" class="" data-parent="#accordion1" data-toggle="collapse" href="" id="accordion1-card-head-locyysyh" role="button" style="background: rgb(255, 255, 255); box-sizing: border-box; color: #1a1c24; display: block; font-size: 28px; letter-spacing: -0.02em; line-height: 1; padding: 28px 50px 28px 30px; position: relative; text-decoration-line: none; transition: all 0.3s ease-in-out 0s;">
+<img alt="" class="card-icon" src="<?php echo $parceljson['response']['data']['events']['0']['courier']['logo_image']['path']; ?>" style="border-style: none;box-sizing: border-box;display: inline-block;height: auto;margin-bottom: 8px;margin-right: 32px;max-width: 100%;vertical-align: middle;background-color: ;width: 10%;border-radius:10px;" width="35"><span class="title" style="box-sizing: border-box; padding-right: 10px;">Trace ekart Parcel</span>
+<span class="card-subtitle" style="box-sizing: border-box; color: #909194; display: inline-block; font-size: 14px; letter-spacing: 0px; line-height: 20px; margin-bottom: 0px; margin-top: 2px;">
+<div class="agogo-app" id="app" style="box-sizing: border-box;">
+<div class="agogo-body" id="body" style="box-sizing: border-box;">
+<div class="agogo-content" style="box-sizing: border-box; padding: 0px 14px;">
+</div>
+<div class="result-body" style="align-items: flex-start; box-sizing: border-box; display: flex; justify-content: space-between; padding-bottom: 20px;">
+<div class="result-content" style="box-sizing: border-box; max-width: 903px;">
+<div class="agogo-content agogo-translate-wrap" style="box-sizing: border-box; padding: 0px;"></div>
+<div class="reult-detail" style="box-sizing: border-box; padding-bottom: 30px;">
+<div class="result-list" style="box-sizing: border-box; color: #999999; font-size: 16px;">
+<ul style="box-sizing: border-box; list-style: none; margin: 0px; padding: 40px 0px 0px;">
+<?php
+foreach($parceljson['response']['data']['events'] as $value){
+?>
+<li class="cur" style="align-items: center; box-sizing: border-box; color: #444444; display: flex; padding-bottom: 50px;">
+<div class="date-bar " style="box-sizing: border-box; min-width: 90px; text-align: right;">
+<p class="date" style="box-sizing: border-box; font-size: 14px; margin: 0px;">Status</p>
+</div>
+<div class="icon agogofont agdownbtn agsuccess" style="-webkit-font-smoothing: antialiased; box-sizing: border-box; color: #2ec106; display: inline-block; font-family: agogofont !important; font-size: 24px; line-height: 1; margin: 0px 40px 0px 30px; position: relative;"></div>
+<div class="content" style="box-sizing: border-box; flex: 1 1 0%; word-break: break-all;width:100%;"><b><?php echo $value['status']; ?></b></div>
+</li>
+<li class="cur" style="align-items: center; box-sizing: border-box; color: #444444; display: flex; padding-bottom: 50px;">
+<div class="date-bar " style="box-sizing: border-box; min-width: 90px; text-align: right;">
+<p class="date" style="box-sizing: border-box; font-size: 14px; margin: 0px;">Location</p>
+</div>
+<div class="icon agogofont agdownbtn agsuccess" style="-webkit-font-smoothing: antialiased; box-sizing: border-box; color: #2ec106; display: inline-block; font-family: agogofont !important; font-size: 24px; line-height: 1; margin: 0px 40px 0px 30px; position: relative;"></div>
+<div class="content" style="box-sizing: border-box; flex: 1 1 0%; word-break: break-all;width:100%;"><?php echo $value['location']; ?></div>
+</li>
+<li class="cur" style="align-items: center; box-sizing: border-box; color: #444444; display: flex; padding-bottom: 50px;">
+<div class="date-bar " style="box-sizing: border-box; min-width: 90px; text-align: right;">
+<p class="date" style="box-sizing: border-box; font-size: 14px; margin: 0px;">Date time</p>
+</div>
+<div class="icon agogofont agdownbtn agsuccess" style="-webkit-font-smoothing: antialiased; box-sizing: border-box; color: #2ec106; display: inline-block; font-family: agogofont !important; font-size: 24px; line-height: 1; margin: 0px 40px 0px 30px; position: relative;"></div>
+<div class="content" style="box-sizing: border-box; flex: 1 1 0%; word-break: break-all;width:100%;"><?php echo $value['datetime']; ?></div>
+</li>
+<hr>
+<?php
+}
+}
+?>
+</ul>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</span></a></div><a aria-controls="accordion1-card-body-kgiydcoi" aria-expanded="true" class="" data-parent="#accordion1" data-toggle="collapse" href="" id="accordion1-card-head-locyysyh" role="button" style="background: rgb(255, 255, 255); box-sizing: border-box; color: #1a1c24; display: block; font-size: 28px; letter-spacing: -0.02em; line-height: 1; padding: 28px 50px 28px 30px; position: relative; text-decoration-line: none; transition: all 0.3s ease-in-out 0s;">
+</a></div><a aria-controls="accordion1-card-body-kgiydcoi" aria-expanded="true" class="" data-parent="#accordion1" data-toggle="collapse" href="" id="accordion1-card-head-locyysyh" role="button" style="background: rgb(255, 255, 255); box-sizing: border-box; color: #1a1c24; display: block; font-size: 28px; letter-spacing: -0.02em; line-height: 1; padding: 28px 50px 28px 30px; position: relative; text-decoration-line: none; transition: all 0.3s ease-in-out 0s;">
+</a>
+</article> </div>
+</div>
+</section>
+<div class="content-wrapper main-content clear-fix">
+<div class="container">
+<section class="speedpost-tracking" id="sms-tracking">
+<h2>Using Speed Post SMS Tracking</h2>
+<p>
+India post also allows you Article tracking using SMS.<br>
+for example To track article No.EE223456782IN <br>
+</p>
+<code><strong>POST TRACK EE123456789IN</strong> (** send to 166 or to 51969 number )</code>
+<p>
+In case you want to track e-MONEY ORDER (e-MO). You need to send SMS in below format to 166 or to 51969 number
+</p>
+<code><strong>POST TRACK 222256789123456789</strong> (** send to 166 or to 51969 number )</code> <br>
+<ul>
+<li>The SMS is case sensitive. Use all CAPITAL LETTERS only.</li>
+<li>Status will be available for items booked up to 60 days from the date of SMS enquiry.</li>
+<li>The service is available from all the service providers in the Country.</li>
+<li>Rates charged by the Service provider are applicable.</li>
+</ul>
+</section>
+<section>
+<h2>Advantages of India Speedpost</h2>
+<p>SpeedPost is a reliable very courier service provided by India Post. This service was launched in August 1986 and it one of the best service among the services that offer assured fast delivery across the region. You can dispatch Letters, Parcels, Gifts, Documents, Articles and similar items in India but also across the world.</p>
+<ul>
+<li>Affordable prices</li>
+<li>Assured delivery, with compensation for lapses</li>
+<li>Highly responsive attitude to customers</li>
+<li>Delivery intimation/Status check</li>
+<li>Fast and Time bound delivery</li>
+<li>Global coverage</li>
+<li>Online tracking facility</li>
+</ul>
+<p>Department Of India Post also comes with many saving Schemes. Offered Saving Schemes are</p>
+<ul>
+<li>Post Office Savings Account</li>
+<li>5-Year Post Office Recurring Deposit Account (RD)</li>
+<li>Post Office Time Deposit Account (TD)</li>
+<li>Post Office Monthly Income Scheme Account (MIS)</li>
+<li>Senior Citizen Savings Scheme (SCSS)&ZeroWidthSpace;</li>
+<li>&ZeroWidthSpace;15 year Public Provident Fund Account (PPF&ZeroWidthSpace;)&ZeroWidthSpace;</li>
+<li>National Savings Certificates (NSC)&ZeroWidthSpace;</li>
+<li>Kisan Vikas Patra&nbsp;(KVP&ZeroWidthSpace;)</li>
+<li>&ZeroWidthSpace;Sukanya Samriddhi Accounts</li>
+</ul>
+<p>
+Speedpost services are avaiblae in all popular Indian Cities like Gurgaon,Chennai, Bangalore, Mumbai, Delhi, Kolkata, Hyderabad, Patna, Nagpur, Lucknow,
+Jaipur, Pondicherry, Allahabad, Ahmadabad, Amritsar, Bhopal, Bhubaneswar Calcutta, Calicut Chandigarh, Cochin, Pune, Noida, Trivandrum, Madurai,
+Trichy and other cities as well.
+</p>
+</section>
+</div>
+</div>
+</main>
+<footer>
+<div class="content-wrapper">
+<h5 style="color:#fff;"></h5>
+<div class="float-left">
+<ul id="footer-menu" itemscope="" itemtype="http://www.schema.org/SiteNavigationElement">
+<li itemprop="name">© 2022 <a style="color:#fff;font-weight:600;" itemprop="url" href="">Parcel Track</a></li>
+<li itemprop="name"><a style="color:#fff;font-weight:600;" itemprop="url" href="#">Contact</a></li>
+<li itemprop="name"><a style="color:#fff;font-weight:600;" itemprop="url" href="#">Privacy Policy</a></li>
+</ul>
+</div>
+</div>
+</footer>
+</body></html>
